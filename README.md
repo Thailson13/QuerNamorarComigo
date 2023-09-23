94f70a915430ce82b996d4de0541830122e83969
Thailson Júlio <thailson-games-2002@hotmail.com>
2023-09-22 21:36:49 -0300

    General improvement in the project

--- README.md
+++ README.md
@@ -6,11 +6,11 @@
 
 Projeto de pedido de namoro em HTML, CSS e JavaScript
 
-Este projeto é um pedido de namoro simples e criativo, utilizando apenas HTML, CSS e JavaScript. O objetivo é que a pessoa amada não possa dizer não ao pedido, pois o botão "não" irá teletransportar para outro local quando ela se aproximar dele.
+Este projeto é um pedido de namoro simples e criativo, utilizando apenas HTML, CSS e JavaScript. O objetivo é que a pessoa amada não possa dizer não ao pedido, pois o botão "não" irá desaparecer quando ela se aproximar dele.
 
 Como funciona?
 
-O projeto consiste em uma página web com um botão "sim" e um botão "não". Quando a pessoa amada clica ou se aproxima do botão "não", ele é teletransportado para outro local. O botão "sim" permanece visível, permitindo que ela diga sim ao pedido.
+O projeto consiste em uma página web com um botão "sim" e um botão "não". Quando a pessoa amada clica ou se aproxima do botão "não", ele desaparece. O botão "sim" permanece visível, permitindo que ela diga sim ao pedido.
 
 Código
 
--- js/main.js
+++ js/main.js
@@ -1,24 +1,15 @@
 const btn = document.querySelector('#my-button');
 const container = document.querySelector('.container-btn');
 
-function randomPosition() {
-  const containerWidth = container.clientWidth;
-  const containerHeight = container.clientHeight;
-  const btnWidth = btn.clientWidth;
-  const btnHeight = btn.clientHeight;
-
-  // Gera duas coordenadas aleatórias dentro do contêiner
-  const randomX = Math.floor(Math.random() * 500);
-  const randomY = Math.floor(Math.random() * 500);
-
-  // Define a posição do botão para as coordenadas aleatórias
-  btn.style.left = `${randomX}px`;
-  btn.style.top = `${randomY}px`;
+function hiddenButton() {
+ btn.style.display = "none";
+  function showButton(){
+    btn.style.display = "block";
+  } setTimeout(showButton, 1000)
 }
 
-// Chama a função randomPosition() quando o mouse entra no botão
-container.onclick = randomPosition;
-btn.onclick = randomPosition;
+container.onclick = hiddenButton;
+btn.onclick = hiddenButton;
 
-container.onmouseenter = randomPosition;
-btn.onmouseenter = randomPosition;
+container.onmouseenter = hiddenButton;
+btn.onmouseenter = hiddenButton;
